dff305bfe6b1e643b1bdfec8ecb959cec38845b3
Mario Trerotola <mariotrerotola@gmail.com>
2018-03-07 19:26:44 +0100

    Initial Commit

---
+++ Sways/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sways
+//
+//  Created by Mario Trerotola on 07/03/2018.
+//  Copyright © 2018 Mario Trerotola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
